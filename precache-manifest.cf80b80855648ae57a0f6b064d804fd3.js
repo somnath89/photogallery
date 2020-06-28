2c2834e2cca732717ae515e6781e2808399beb3c
somnath89 <somnathsengupta845@yahoo.com>
2020-06-29 02:01:03 +0530

    Updates

---
+++ precache-manifest.cf80b80855648ae57a0f6b064d804fd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f266f43850b647052521",
+    "url": "/photogallery/static/js/runtime~main.f266f438.js"
+  },
+  {
+    "revision": "e58f9f90c55f014a5fd2",
+    "url": "/photogallery/static/js/main.e58f9f90.chunk.js"
+  },
+  {
+    "revision": "1505da966eee238749b4",
+    "url": "/photogallery/static/js/1.1505da96.chunk.js"
+  },
+  {
+    "revision": "e58f9f90c55f014a5fd2",
+    "url": "/photogallery/static/css/main.e74b6af0.chunk.css"
+  },
+  {
+    "revision": "70064a02d40afe23d3d6a42378dc1d8c",
+    "url": "/photogallery/index.html"
+  }
+];
